a949ea6cc90e3893bc872bee73d4bb6c1dbaf6c5
yateng1234 <437879403@qq.com>
2019-02-01 14:45:40 +0800

    test

--- src/api/sys/login/index.js
+++ src/api/sys/login/index.js
@@ -2,7 +2,7 @@ import request from '@/plugin/axios'
 
 export function AccountLogin (data) {
   return request({
-    url: request.adornUrl('/sys/login'),
+    url: request.adornUrl('/sys/login1'),
     method: 'post',
     data
   })
